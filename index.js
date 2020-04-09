ddfc71ad45015bca3f9f638b1e24bfdf4f2e9b38
Frank.Wu <Frank.Wu@Bionime.com>
2020-04-09 18:11:39 +0800

    test

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ var bot = linebot({
     console.log(event); //把收到訊息的 event 印出來看看
     if(event.message.type == 'text'){
       bot.reply(event.replyToken,"你是不是說了=>"+ event.message.text)
-      console.log(bot.getUserProfile(event.source.userId))
+      bot.userId
+      console.log("file => "+bot.getUserProfile(event.source.userId))
+      console.log("uid => " +bot.userId)
       bot.push(event.source.userId,"嗨 " +bot.getUserProfile(event.source.userId).displayName)
     }
   });
