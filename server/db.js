32598cd833ca52361f1ad6c0d3beed7d33b1b575
idick7069 <Frank.Wu@Bionime.com>
2020-04-27 15:09:54 +0800

    修改DB位置

--- server/db.js
+++ server/db.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://localhost:27017/linebot');
-mongoose.connect('mongodb://heroku_m7jzgxp5:heroku_m7jzgxp5@ds215502.mlab.com:15502/heroku_m7jzgxp5')
+if(process.env.MONGODB_URI === undefined){
+    mongoose.connect('mongodb://localhost:27017/linebot');
+}else{  
+mongoose.connect(process.env.MONGODB_URI)
+}
 const db = mongoose.connection;
 db.on('error', err => console.error('connection error', err));
 db.once('open', db => console.log('Connected to MongoDB'))
